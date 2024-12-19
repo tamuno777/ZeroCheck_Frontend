6db2ac376b0df6e0e98d4da80721ae8a5f8aa8d8
Ojukwu_Levi <132361582+Levi-Ojukwu@users.noreply.github.com>
2024-12-19 10:30:25 +0100

    Build the nft certification component. (#19)
    * FIXES: https://github.com/Explore-Beyond-Innovations/ZeroCheck_Frontend/issues/6
    
    * UPDATE: Created the NftCertification component
    
    * UPDATE: Completed the NftCertification UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import type { ISuccessResult } from "@worldcoin/idkit";
 import { verify } from "./actions/verify/verify";
 
 export default function Home() {
+
   const app_id = process.env.NEXT_PUBLIC_WLD_APP_ID as `app_${string}`;
   const action = process.env.NEXT_PUBLIC_WLD_ACTION;
 
